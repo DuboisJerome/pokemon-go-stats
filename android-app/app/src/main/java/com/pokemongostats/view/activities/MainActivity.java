0e64564cea5f8a4b56c4435310ebee9f161a8f19
Jérôme Dubois <jerome.dubois.etu@gmail.com>
2017-04-05 01:23:38 +0200

    Slide menu

--- android-app/app/src/main/java/com/pokemongostats/view/activities/MainActivity.java
+++ android-app/app/src/main/java/com/pokemongostats/view/activities/MainActivity.java
@@ -6,14 +6,15 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -26,35 +27,41 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.pokemongostats.R;
 import com.pokemongostats.controller.services.OverlayService;
 import com.pokemongostats.controller.utils.TagUtils;
-import com.pokemongostats.model.bean.DrawerItem;
 import com.pokemongostats.view.PkmnGoStatsApplication;
-import com.pokemongostats.view.adapters.CustomDrawerAdapter;
+import com.pokemongostats.view.fragments.DefaultFragment;
 import com.pokemongostats.view.fragments.DmgSimuFragment;
 import com.pokemongostats.view.fragments.PokedexFragment;
-import com.pokemongostats.view.fragments.DefaultFragment;
 import com.pokemongostats.view.utils.PreferencesUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Zapagon
  */
 public class MainActivity extends AppCompatActivity {
 
-    private String[] mMenuTitles;
+    private NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private ListView mDrawerList;
+    private Toolbar toolbar;
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the fragments
+    public static String TAG_POKEDEX = PokedexFragment.class.getSimpleName();
+    public static String TAG_SIMULATOR = DmgSimuFragment.class.getSimpleName();
+    public static String TAG_SETTINGS = "settings";
+    public static String NEXT_TAG = TAG_POKEDEX;
+    public static String LAST_TAG = "";
+
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
+
+    private Handler mHandler;
+
     private DefaultFragment mCurrentFragment;
 
     private OverlayService service;
@@ -85,9 +92,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    private int lastPosition = -1;
-    private CharSequence mTitle, mDrawerTitle;
-    private List<DrawerItem> mMenus;
 
     public OverlayService getService() {
         return service;
@@ -96,12 +100,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_one_fragment);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         ActionBar supportActionBar = getSupportActionBar();
         if (supportActionBar != null) {
             supportActionBar.setDisplayShowHomeEnabled(true);
@@ -110,41 +112,22 @@ public class MainActivity extends AppCompatActivity {
             supportActionBar.setHomeActionContentDescription(R.string.home_btn_desc);
         }
 
-        mMenuTitles = getResources().getStringArray(R.array.menu_array);
+        mHandler = new Handler();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setItemIconTintList(null);
 
-        mMenus = initMenus();
+        // load toolbar titles from string resources
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
 
-        mTitle = mDrawerTitle = getTitle();
+        // initializing navigation menu
+        setUpNavigationView();
 
-        CustomDrawerAdapter a = new CustomDrawerAdapter(this, R.layout.drawer_list_item, mMenus);
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(a);
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.string.drawer_open, R.string.drawer_close) {
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        registerReceiver(exitReceiver, new IntentFilter("EXIT"));
-        selectMenu(0);
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            NEXT_TAG = TAG_POKEDEX;
+            loadFragment();
+        }
     }
 
     @Override
@@ -188,6 +171,10 @@ public class MainActivity extends AppCompatActivity {
         });
         tb.getMenu().findItem(R.id.action_is_last_evolution_only)
                 .setChecked(PreferencesUtils.isLastEvolutionOnly(getApplicationContext()));
+
+        if (navItemIndex == 0) {
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+        }
         return true;
     }
 
@@ -195,9 +182,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Pass the event to ActionBarDrawerToggle, if it returns
         // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
+//        if (mDrawerToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
 
         Context c = getApplicationContext();
         AlertDialog dialog;
@@ -246,22 +233,6 @@ public class MainActivity extends AppCompatActivity {
                 item.setChecked(!item.isChecked());
                 PreferencesUtils.setLastEvolutionOnly(c, item.isChecked());
                 break;
-            case R.id.action_about:
-                final Spanned s = Html.fromHtml(c.getString(R.string.about_content));
-
-                dialog = new AlertDialog.Builder(this).setTitle(c.getString(R.string.about))
-                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.dismiss();
-                            }
-                        })
-                        .setMessage(s).setCancelable(true).create();
-                dialog.show();
-                // Make the textview clickable. Must be called after show()
-                ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
-
-                break;
             case R.id.action_minimize:
                 if (getService() != null) {
                     getService().minimize();
@@ -274,110 +245,219 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectMenu(position);
-        }
-    }
-
-    private List<DrawerItem> initMenus(){
-        List<DrawerItem> menus = new ArrayList<>();
-        menus.add(new DrawerItem(getString(R.string.pokedex)));
-        menus.add(new DrawerItem(getString(R.string.simulator)));
-        menus.add(new DrawerItem(getString(R.string.settings), android.R.drawable.ic_menu_preferences));
-        menus.add(new DrawerItem(getString(R.string.about), android.R.drawable.ic_menu_help));
-        return menus;
-    }
-
-    private void selectMenu(int position) {
-        if (lastPosition == position) {
-            return;
-        }
-        switch (position) {
-            case 0:
-                showPokedexFragment();
-                break;
-            case 1:
-                showSimulatorFragment();
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-        }
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mMenuTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-        lastPosition = position;
-    }
-
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        //
-
-        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
     @Override
     public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawers();
+            return;
+        }
         mCurrentFragment.onBackPressed();
     }
 
-    public void showPokedexFragment(){
-        String tag = PokedexFragment.class.getName();
+    public DefaultFragment getPokedexFragment(final FragmentTransaction ft) {
+        String tag = TAG_POKEDEX;
         FragmentManager fm = getSupportFragmentManager();
         PokedexFragment f = (PokedexFragment) fm.findFragmentByTag(tag);
-        FragmentTransaction fTransaction = fm.beginTransaction();
         if (null == f) {
             // create Fragment
             f = new PokedexFragment();
-            fTransaction.addToBackStack(tag);
+            ft.addToBackStack(tag);
             Log.i(TagUtils.DEBUG, "Create PokedexFragment");
         } else {
             Log.i(TagUtils.DEBUG, "Pop PokedexFragment");
         }
-        fTransaction.replace(R.id.fragment_container, f, tag);
-        fTransaction.show(f);
-        fTransaction.commit();
-        mCurrentFragment = f;
+        return f;
     }
 
-    public void showSimulatorFragment(){
-        String tag = DmgSimuFragment.class.getName();
+    public DefaultFragment getSimulatorFragment(final FragmentTransaction ft) {
+        String tag = TAG_SIMULATOR;
         FragmentManager fm = getSupportFragmentManager();
         DmgSimuFragment f = (DmgSimuFragment) fm.findFragmentByTag(tag);
-        FragmentTransaction fTransaction = fm.beginTransaction();
         if (null == f) {
             // create Fragment
             f = new DmgSimuFragment();
-            fTransaction.addToBackStack(tag);
+            ft.addToBackStack(tag);
             Log.i(TagUtils.DEBUG, "Create DmgSimuFragment");
         } else {
             Log.i(TagUtils.DEBUG, "Pop DmgSimuFragment");
         }
-        fTransaction.replace(R.id.fragment_container, f, tag);
-        fTransaction.show(f);
-        fTransaction.commit();
-        mCurrentFragment = f;
+        return f;
+    }
+
+    public DefaultFragment getSettingsFragment(final FragmentTransaction ft) {
+        String tag = TAG_SETTINGS;
+        FragmentManager fm = getSupportFragmentManager();
+        PokedexFragment f = (PokedexFragment) fm.findFragmentByTag(tag);
+        if (null == f) {
+            // create Fragment
+            f = new PokedexFragment();
+            ft.addToBackStack(tag);
+            Log.i(TagUtils.DEBUG, "Create PokedexFragment");
+        } else {
+            Log.i(TagUtils.DEBUG, "Pop PokedexFragment");
+        }
+        return f;
+    }
+
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation mDrawerLayout
+        if (LAST_TAG.equals(NEXT_TAG)) {
+            mDrawerLayout.closeDrawers();
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                final FragmentManager fm = getSupportFragmentManager();
+                final FragmentTransaction fTransaction = fm.beginTransaction();
+                final DefaultFragment f = getOrCreateFragment(fTransaction);
+                fTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fTransaction.replace(R.id.fragment_container, f, NEXT_TAG);
+                fTransaction.show(f);
+                fTransaction.commitAllowingStateLoss();
+                mCurrentFragment = f;
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        //Closing mDrawerLayout on item click
+        mDrawerLayout.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private DefaultFragment getOrCreateFragment(final FragmentTransaction ft) {
+        DefaultFragment f;
+        switch (navItemIndex) {
+            case 0:
+                // pokedex
+                f = getPokedexFragment(ft);
+                break;
+            case 1:
+                // simulator
+                f = getSimulatorFragment(ft);
+                break;
+            case 2:
+                f = getSettingsFragment(ft);
+                break;
+            default:
+                f = getPokedexFragment(ft);
+        }
+        return f;
+    }
+
+    private void setToolbarTitle() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+        }
+    }
+
+    private void selectNavMenu() {
+        mNavigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_pokedex:
+                        navItemIndex = 0;
+                        NEXT_TAG = TAG_POKEDEX;
+                        break;
+                    case R.id.nav_simulator:
+                        navItemIndex = 1;
+                        NEXT_TAG = TAG_SIMULATOR;
+                        break;
+                    case R.id.nav_settings:
+                        navItemIndex = 2;
+                        NEXT_TAG = TAG_SETTINGS;
+                        break;
+                    case R.id.nav_about_us:
+                        // launch new intent instead of loading fragment
+                        final Spanned s = Html.fromHtml(getString(R.string.about_content));
+
+                        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).setTitle(getString(R.string.about))
+                                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                })
+                                .setMessage(s).setCancelable(true).create();
+                        dialog.show();
+                        // Make the textview clickable. Must be called after show()
+                        ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadFragment();
+
+                return true;
+            }
+        });
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the mDrawerLayout closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the mDrawerLayout open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to mDrawerLayout layout
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
     }
 }
