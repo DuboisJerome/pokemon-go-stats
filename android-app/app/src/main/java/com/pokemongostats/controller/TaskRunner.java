4e20ccc9857e6a3bf69deac933bb312bbf76eb25
DuboisJerome <jerome.dubois.etu@gmail.com>
2023-04-25 22:10:27 +0200

    Deplacements des icons de Drawable vers un sous répertoire d'assets

---
+++ android-app/app/src/main/java/com/pokemongostats/controller/TaskRunner.java
@@ -0,0 +1,38 @@
+package com.pokemongostats.controller;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TaskRunner {
+	private static final Executor executor =
+			new ThreadPoolExecutor(5, 32, 1,
+					TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+	private final Handler handler = new Handler(Looper.getMainLooper());
+
+	public interface Callback<R> {
+		void onComplete(R result);
+
+		void onError(Exception e);
+	}
+
+	public <R> void executeAsync(Callable<R> callable, Callback<R> callback) {
+		executor.execute(() -> {
+			try {
+				final R result = callable.call();
+				handler.post(() ->
+					callback.onComplete(result)
+				);
+			} catch (Exception e) {
+				handler.post(() ->
+					callback.onError(e)
+				);
+			}
+		});
+	}
+}
--- android-app/app/src/main/java/com/pokemongostats/view/utils/PkmnDrawableCache.java
+++ android-app/app/src/main/java/com/pokemongostats/view/utils/PkmnDrawableCache.java
@@ -1,15 +1,17 @@
 package com.pokemongostats.view.utils;
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 
 import androidx.collection.LruCache;
 import androidx.core.content.ContextCompat;
 
+import com.pokemongostats.controller.TaskRunner;
 import com.pokemongostats.model.bean.bdd.PkmnDesc;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -29,7 +31,9 @@ public class PkmnDrawableCache {
 		Drawable d = cachedPkmnDrawables.get(DEFAULT_DRAWABLE_ID);
 		if (d == null) {
 			d = ContextCompat.getDrawable(c, android.R.drawable.ic_menu_help);
-			cachedPkmnDrawables.put(DEFAULT_DRAWABLE_ID, d);
+			if(d != null){
+				cachedPkmnDrawables.put(DEFAULT_DRAWABLE_ID, d);
+			}
 		}
 		return d;
 	}
@@ -38,18 +42,19 @@ public class PkmnDrawableCache {
 		if (c == null) {
 			return null;
 		}
+
 		String id = PkmnDrawableCache.getCacheId(pokedexNum, form);
-		// pre execute
-		String uri = "@drawable/pokemon_" + id;
-		String packageName = c.getPackageName();
-		int imgRes = c.getResources().getIdentifier(uri, null, packageName);
 
 		Drawable d = cachedPkmnDrawables.get(id);
 		if (d == null) {
+			AssetManager assetManager = c.getAssets();
 			try {
-				d = ContextCompat.getDrawable(c, imgRes);
-				cachedPkmnDrawables.put(id, d);
-			} catch (Resources.NotFoundException e) {
+				InputStream ims = assetManager.open("iconspkmn/pokemon_" + id+".png");
+				d = Drawable.createFromStream(ims, null);
+				if(d != null) {
+					cachedPkmnDrawables.put(id, d);
+				}
+			} catch (IOException ex) {
 				// tente de recupérer la forme normal
 				if (!form.toLowerCase(Locale.getDefault()).equals("normal")) {
 					d = get(c, pokedexNum, "normal");
@@ -67,35 +72,18 @@ public class PkmnDrawableCache {
 	}
 
 	public static void getAsync(Context c, PkmnDesc p, Consumer<Drawable> consumer) {
-		new PkmnDrawableAsyncTask(c, p, consumer).execute();
-	}
+		WeakReference<Context> wc= new WeakReference<>(c);
+		new TaskRunner().executeAsync(() -> PkmnDrawableCache.get(wc.get(), p),
+				new TaskRunner.Callback<>() {
+					@Override
+					public void onComplete(Drawable d) {
+						consumer.accept(d);
+					}
 
-	static class PkmnDrawableAsyncTask extends AsyncTask<Object,Object,Drawable> {
-		private final WeakReference<Context> c;
-		private final PkmnDesc pkmnDesc;
-		private final Consumer<Drawable> consumer;
-
-		private PkmnDrawableAsyncTask(Context c, PkmnDesc pkmnDesc, Consumer<Drawable> consumer) {
-			super();
-			this.c = new WeakReference<>(c);
-			this.pkmnDesc = pkmnDesc;
-			this.consumer = consumer;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			// Load drawable
-			//Drawable d = PkmnDrawableCache.get(c, pkmnDesc);
-		}
-
-		@Override
-		protected Drawable doInBackground(Object[] objects) {
-			return PkmnDrawableCache.get(this.c.get(), this.pkmnDesc);
-		}
-
-		@Override
-		protected void onPostExecute(Drawable d) {
-			this.consumer.accept(d);
-		}
+					@Override
+					public void onError(Exception e) {
+						throw new RuntimeException(e);
+					}
+				});
 	}
 }
